db51f3405e6fa12b27b5741395823aa2fab963fe
Rajat Sharma <rxx175@gmail.com>
2022-08-17 10:24:48 +0530

    Update switch

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -19,74 +19,52 @@ export default {
 </script>
 
 <template>
-   <label class="app-switch">
-      <input 
-        type="checkbox" 
-        :id="id" 
-        :checked="value"
-        @change="handleChanges" />
-      <span class="app-switch-slider round"></span>
-   </label>
+  <div class="app-switch">
+    <input 
+      class="app-switch-input" 
+      type="checkbox" 
+      :id="id"
+      :checked="value"
+      @change="handleChanges" />
+    <label class="app-switch-label" :for="id"></label>
+  </div>
 </template>
 
 <style>
 .app-switch {
-  position: relative;
-  display: inline-block;
-  width: 50px;
-  height: 28px;
+  display: inline-flex;
+  margin: 0 10px;
 }
-
-.app-switch input { 
-  opacity: 0;
-  width: 0;
+.app-switch input[type=checkbox] {
   height: 0;
+  width: 0;
+  visibility: hidden;
 }
-
-.app-switch-slider {
-  position: absolute;
+.app-switch input[type=checkbox]:checked + label {
+  background: var(--color);
+}
+.app-switch input[type=checkbox]:checked + label::after {
+  left: calc(100% - 4px);
+  transform: translateX(-100%);
+}
+.app-switch label {
   cursor: pointer;
-  top: 0;
-  left: 0;
-  right: -4px;
-  bottom: 0;
-  background-color: var(--accents-2);
-  -webkit-transition: 230ms;
-  transition: 230ms;
+  width: 48px;
+  height: 24px;
+  background: var(--accents-2);
+  display: block;
+  border-radius: 24px;
+  position: relative;
 }
-
-.app-switch-slider:before {
-  position: absolute;
+.app-switch label::after {
   content: "";
-  height: 20px;
-  width: 20px;
+  position: absolute;
+  top: 4px;
   left: 4px;
-  bottom: 4px;
-  background-color: var(--accents-0);
-  -webkit-transition: 230ms;
-  transition: 230ms;
-}
-
-input:checked + .app-switch-slider {
-  background-color: var(--color);
-}
-
-input:focus + .app-switch-slider {
-  box-shadow: 0 0 1px var(--color);
-}
-
-input:checked + .app-switch-slider:before {
-  -webkit-transform: translateX(26px);
-  -ms-transform: translateX(26px);
-  transform: translateX(26px);
-}
-
-/* Rounded sliders */
-.app-switch-slider.round {
-  border-radius: 34px;
-}
-
-.app-switch-slider.round:before {
-  border-radius: 50%;
+  width: 16px;
+  height: 16px;
+  background: var(--accents-0);
+  border-radius: 16px;
+  transition: 0.3s;
 }
 </style>
