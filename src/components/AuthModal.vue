8c575cd1b40991bf183bbe611b938cbb85a3f21f
Rajat Sharma <rxx175@gmail.com>
2022-08-21 00:45:21 +0530

    written <AuthModel />

---
+++ src/components/AuthModal.vue
@@ -0,0 +1,43 @@
+<script>
+import { defineComponent } from 'vue';
+import Modal from './Modal.vue';
+import { setValue } from '../utils/store';
+
+export default defineComponent({
+   name: 'AuthModal',
+   data() {
+      return {
+         token: '',
+      }
+   },
+   components: { Modal },
+   methods: {
+      saveToken() {
+         if (this.token.length < 1) {
+            return;
+         }
+         setValue('token', this.token);
+         this.$emit('close');
+      }
+   }
+});
+</script>
+
+<template>
+   <Modal title="Authentication Token">
+      <input 
+         type="password" 
+         class="input-controller secret-token-input"
+         v-model="token" />
+
+      <template #footer>
+         <app-button fill="primary" @click="saveToken">Done</app-button>
+      </template>
+   </Modal>
+</template>
+
+<style>
+.secret-token-input {
+   width: calc(100% - 24px);
+}
+</style>
