cc1ab6adbe5a17dcb028ea865b3240817dcb0260
Rajat Sharma <rxx175@gmail.com>
2022-08-21 00:50:07 +0530

    update settings view

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,5 +1,7 @@
 <script>
 import { defineComponent } from 'vue';
+import { uploadPushSubscription } from '../services/push-subs';
+import { createToast } from '../utils/toast';
 
 export default defineComponent({
     name: 'SettingView',
@@ -13,13 +15,24 @@ export default defineComponent({
             console.log("CALL", value);
 
             if (value) {
+                const pushPayload = {};
+
+                // @ts-ignore
                 const applicationServerKey = import.meta.env.VITE_APP_APPLICATION_SERVER_KEY;
                 const sw = await window.navigator.serviceWorker.ready;
                 let push = await sw.pushManager.subscribe({
                     userVisibleOnly: true,
                     applicationServerKey,
                 });
-                console.log("ROW", JSON.stringify(push));
+                pushPayload.user_agent = window.navigator.userAgent;
+                pushPayload.payload = JSON.stringify(push);
+
+                try {
+                    await uploadPushSubscription(pushPayload);
+                    createToast('primary', 'Push notification enabled');
+                } catch (error) {
+                    createToast('error', error.message);
+                }
             } else {
 
             }
@@ -51,7 +64,6 @@ export default defineComponent({
     align-items: center;
     justify-content: space-between;
     gap: 12px;
-    padding: 8px;
-    border-bottom: 1px solid var(--accents-2);
+    padding: 18px;
 }
 </style>
