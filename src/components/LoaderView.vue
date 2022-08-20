0188cbe9b7699aa5114ee832d7afa037a39b72f5
Rajat Sharma <rxx175@gmail.com>
2022-08-21 00:48:02 +0530

    added <LoaderView />

---
+++ src/components/LoaderView.vue
@@ -0,0 +1,33 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+   name: 'LoaderView',
+   props: {
+      message: {
+         type: String,
+         required: false,
+      }
+   }
+});
+</script>
+
+<template>
+   <div class="app-loader-view">
+      <app-loader size="28px"></app-loader>
+      <p v-if="message"><small>{{ message }}</small></p>
+   </div>
+</template>
+
+<style>
+.app-loader-view {
+   padding-top: 12px;
+   padding-bottom: 12px;
+   min-height: 340px;
+   gap: 20px;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+}
+</style>
