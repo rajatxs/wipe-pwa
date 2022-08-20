b2d12874166af6ed08c52dee05d6ca2a5095ef9a
Rajat Sharma <rxx175@gmail.com>
2022-08-21 00:49:00 +0530

    register app-loader component

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router';
 import Button from './components/Button.vue';
 import IconButton from './components/IconButton.vue';
 import Switch from './components/Switch.vue';
+import Loader from './components/Loader.vue';
 
 import './assets/main.css';
 import './assets/custom.css';
@@ -13,6 +14,7 @@ const app = createApp(App);
 app.component('app-button', Button);
 app.component('app-icon-button', IconButton);
 app.component('app-switch', Switch);
+app.component('app-loader', Loader);
 
 app.use(router);
 app.mount('#app');
