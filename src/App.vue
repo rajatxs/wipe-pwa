f26d205170c9cb7b9214fc4615c8e18fe52e5be7
Rajat Sharma <rxx175@gmail.com>
2022-08-21 00:50:23 +0530

    use auth modal

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
-<script setup>
+<script>
+import { defineComponent } from 'vue';
+import AuthModal from './components/AuthModal.vue';
 import { RouterView } from 'vue-router';
 import Header from './components/Header.vue';
 import PWALoader from './components/PWALoader.vue';
+import { generateTag } from './utils/common';
+import { hasValue, setValue } from './utils/store';
+
+export default defineComponent({
+  name: 'App',
+  data() {
+    return {
+      requireToken: false,
+    }
+  },
+  components: {
+    AuthModal,
+    Header,
+    PWALoader,
+    RouterView,
+  },
+  created() {
+    if (!hasValue('tag')) {
+      setValue('tag', generateTag());
+    }
+
+    if (!hasValue('token')) {
+      this.requireToken = true;
+    }
+  }
+})
 </script>
 
 <template>
@@ -9,6 +37,9 @@ import PWALoader from './components/PWALoader.vue';
   <Header></Header>
   <main class="app-main">
     <RouterView />
+    <AuthModal 
+      v-if="requireToken"
+      @close="requireToken = false" />
   </main>
 </template>
 
