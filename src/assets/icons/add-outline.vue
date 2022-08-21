d906dd68a8e2849705d272ff60fa19ffff2f019e
Rajat Sharma <rxx175@gmail.com>
2022-08-21 19:11:22 +0530

    update assets

--- src/assets/icons/add-outline.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="app-icons"
-      viewBox="0 0 512 512">
-      <title>Add</title>
-      <path d="M256 112v288M400 256H112" />
-    </svg>
-</template>
