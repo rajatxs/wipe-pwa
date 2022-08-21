9319588d037ac88b8ea39e4124936b65f796fbae
Rajat Sharma <rxx175@gmail.com>
2022-08-21 19:12:48 +0530

    added config

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// @ts-nocheck
+import { getValue } from './utils/store';
+
+export const REMOTE_URL_KEY = 'remote-url';
+export const TAG_KEY = 'tag';
+export const AUTH_TOKEN_KEY = 'token';
+
+export const APP_APPLICATION_SERVER_KEY = import.meta.env.VITE_APP_APPLICATION_SERVER_KEY;
+export const appTag = () => getValue(TAG_KEY);
+export const appAuthToken = () => getValue(AUTH_TOKEN_KEY);
+export const appRemoteURL = () => getValue(REMOTE_URL_KEY) || window.location.origin;
