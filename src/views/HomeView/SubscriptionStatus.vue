f09d8ba1402a0926d344744270a3184501b8a77d
Rajat Sharma <rxx175@gmail.com>
2022-08-28 17:43:55 +0530

    update SubscriptionStatus component

--- src/views/HomeView/SubscriptionStatus.vue
+++ src/views/HomeView/SubscriptionStatus.vue
@@ -1,69 +1,86 @@
-<script>
-import { defineComponent } from 'vue';
+<script setup>
+import { ref, watch, defineProps, onMounted, onUnmounted } from 'vue';
 import { $get } from '../../utils/http';
 import moment from 'moment';
 
-export default defineComponent({
-   name: 'AppSubscriptionStatus',
-   data() {
-      return {
-         id: NaN,
-         active: false,
-         timestamp: '',
-         loading: true,
+const UPDATE_INTERVAL = 5000;
+const id = ref(NaN);
+const active = ref(false);
+const timestamp = ref('');
+const loading = ref(true);
+const timer = ref(null);
+const timeString = ref('');
+const props = defineProps({
+   show: Boolean,
+   subId: Number,
+   fallback: String,
+});
+
+async function fetchLastKnownPresence() {
+   let record;
+
+   try {
+      const response = await $get(`/presence?subid=${props.subId}&limit=1`);
+
+      if (Array.isArray(response.result)) {
+         record = response.result[0];
+      } else {
+         record = response.result;
       }
-   },
-   props: {
-      subsId: {
-         type: Number,
-         required: true,
-      },
-      interval: {
-         type: Number,
-         default: 3000,
-      },
-      fallback: {
-         type: String,
-         required: false,
-      },
-   },
-   mounted() {
-      this.fetchLastKnownPresence();
 
-      setInterval(() => {
-         this.fetchLastKnownPresence();
-      }, this.interval);
-   },
-   methods: {
-      async fetchLastKnownPresence() {
-         let subsId = this.subsId;
-         let record;
-
-         try {
-            const response = await $get(`/presence?subid=${subsId}&limit=1`);
-
-            if (Array.isArray(response.result)) {
-               record = response.result[0];
-            } else {
-               record = response.result;
-            }
-
-            if (record) {
-               this.id = record.id;
-               this.active = Boolean(record.status);
-               this.timestamp = record.ts;
-            }
-         } catch (error) {
-            console.error("Couldn't get last seen", error);
-         }
-
-         this.loading = false;
-      },
-   },
-   computed: {
-      readable() {
-         return moment(this.timestamp, true).startOf('minute').fromNow(false);
+      if (record) {
+         id.value = record.id;
+         active.value = Boolean(record.status);
+         timestamp.value = record.ts;
+         timeString.value = moment(record.ts, true)
+            .startOf('minute')
+            .fromNow(false);
       }
+   } catch (error) {
+      console.error("Couldn't get last seen", error);
+   }
+
+   loading.value = false;
+}
+
+function registerInterval() {
+   if (timer.value) {
+      return;
+   }
+
+   timer.value = setInterval(async () => {
+      await fetchLastKnownPresence();
+   }, UPDATE_INTERVAL);
+}
+
+function unregisterInterval() {
+   if (timer.value) {
+      clearInterval(timer.value);
+      timer.value = null;
+   }
+}
+
+watch(
+   () => props.show,
+   (value) => {
+      if (value) {
+         registerInterval();
+      } else {
+         unregisterInterval();
+      }
+   }
+);
+
+onUnmounted(function () {
+   unregisterInterval();
+});
+
+onMounted(async function () {
+   if (props.show) {
+      await fetchLastKnownPresence();
+      registerInterval();
+   } else {
+      loading.value = false;
    }
 });
 </script>
@@ -71,9 +88,9 @@ export default defineComponent({
 <template>
    <div class="app-subs-status">
       <template v-if="loading">...</template>
-      <template v-else-if="active">online</template>
-      <template v-else-if="timestamp">{{readable}}</template>
-      <template v-else>{{fallback}}</template>
+      <template v-else-if="active && show">online</template>
+      <template v-else-if="timestamp && show">{{ timeString }}</template>
+      <template v-else>{{ fallback }}</template>
    </div>
 </template>
 
