f58df2cc7374d60e1bb1b8d2aee2452d3492599b
Rajat Sharma <rxx175@gmail.com>
2022-08-28 22:40:40 +0530

    written BottomActionSheet component

---
+++ src/components/BottomActionSheet.vue
@@ -0,0 +1,23 @@
+<template>
+   <div class="app-button-action-sheet">
+      <slot></slot>
+   </div>
+</template>
+
+<style>
+.app-button-action-sheet {
+   position: fixed;
+   bottom: 0;
+   left: 50%;
+   right: auto;
+   width: calc(var(--scale-x));
+   z-index: 100;
+   background-color: var(--surface);
+   transform: translateX(-50%);
+}
+@media screen and (min-width: 680px) {
+   .app-button-action-sheet {
+      width: calc(var(--scale-x) - 20%);
+   }
+}
+</style>
