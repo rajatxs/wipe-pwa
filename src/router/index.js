1db8bf85995bc1671e67324ae116b62e89df6e89
Rajat Sharma <rxx175@gmail.com>
2022-08-28 17:42:30 +0530

    register Subscription route

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,40 @@
 // @ts-nocheck
 import { createRouter, createWebHashHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import HomeView from '../views/HomeView/HomeView.vue';
 import SettingsView from '../views/SettingsView/SettingsView.vue';
 import AddSubscriptionView from '../views/AddSubscription.vue';
 import WAManageView from '../views/WAManageView.vue';
+import SubscriptionView from '../views/SubscriptionView/SubscriptionView.vue';
 
 const router = createRouter({
-    history: createWebHashHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: HomeView,
-        },
-        {
-            path: '/settings',
-            name: 'settings',
-            component: SettingsView,
-        },
-        {
-            path: '/add',
-            name: 'AddSubscription',
-            component: AddSubscriptionView,
-        },
-        {
-            path: '/wa',
-            name: 'WAManager',
-            component: WAManageView,
-        }
-    ],
+   history: createWebHashHistory(import.meta.env.BASE_URL),
+   routes: [
+      {
+         path: '/',
+         name: 'home',
+         component: HomeView,
+      },
+      {
+         path: '/settings',
+         name: 'settings',
+         component: SettingsView,
+      },
+      {
+         path: '/add',
+         name: 'AddSubscription',
+         component: AddSubscriptionView,
+      },
+      {
+         path: '/wa',
+         name: 'WAManager',
+         component: WAManageView,
+      },
+      {
+         path: '/subs/:id',
+         name: 'Subscription',
+         component: SubscriptionView,
+      },
+   ],
 });
 
 export default router;
