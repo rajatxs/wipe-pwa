8a9edf9b54a6451a19d101abeb9fed619a5c0d63
rajatxs <rxx175@gmail.com>
2023-02-11 22:59:35 +0530

    define loadLastRestartedTime()

--- src/views/WAManageView/WAManageView.vue
+++ src/views/WAManageView/WAManageView.vue
@@ -1,14 +1,18 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
+import moment from 'moment';
 import ServiceStatus from './ServiceStatus.vue';
 import SessionOperation from './SessionOperation.vue';
 import ServiceAction from './ServiceAction.vue';
 import { $get } from '../../utils/http';
 import { createToast } from '../../utils/toast';
+import { getValue } from '../../utils/store';
+import { WA_SERVICE_RESTART_TS_KEY } from '../../config';
 
 const status = ref(false);
 const timer = ref(null);
 const loading = ref(false);
+const restartedTimestamp = ref(null);
 
 async function fetchServiceStatus() {
    try {
@@ -20,9 +24,23 @@ async function fetchServiceStatus() {
    }
 }
 
+function loadLastRestartedTime() {
+   const localRestartTimestamp = getValue(WA_SERVICE_RESTART_TS_KEY);
+
+   if (!localRestartTimestamp) {
+      if (!restartedTimestamp.value) {
+         restartedTimestamp.value = null;
+      }
+      return;
+   }
+
+   restartedTimestamp.value = moment(Number(localRestartTimestamp), true).fromNow(false);
+}
+
 function registerStatusInterval() {
    if (!timer.value) {
       timer.value = setInterval(() => {
+         loadLastRestartedTime();
          fetchServiceStatus();
       }, 2000);
    }
@@ -36,6 +54,7 @@ function unregisterStatusInterval() {
 
 onMounted(async function () {
    loading.value = true;
+   loadLastRestartedTime();
    await fetchServiceStatus();
    loading.value = false;
    registerStatusInterval();
@@ -47,7 +66,10 @@ onUnmounted(function () {
 </script>
 
 <template>
-   <ServiceStatus :status="status" :loading="loading" />
+   <ServiceStatus 
+      :status="status" 
+      :loading="loading"
+      :last-restarted-time="restartedTimestamp" />
    <hr class="status-divider" />
    <SessionOperation />
    <ServiceAction :status="status" />
