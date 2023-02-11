29a883852cdae4822a0f05450f6ffa9de2bc859d
rajatxs <rxx175@gmail.com>
2023-02-11 22:57:28 +0530

    added WA_SERVICE_RESTART_TS_KEY

--- src/config.js
+++ src/config.js
@@ -5,6 +5,7 @@ export const REMOTE_URL_KEY = 'remote-url';
 export const TAG_KEY = 'tag';
 export const AUTH_TOKEN_KEY = 'token';
 export const APP_SERVER_KEY = 'app-server-key';
+export const WA_SERVICE_RESTART_TS_KEY = 'wa-restart-ts';
 
 export const APP_APPLICATION_SERVER_KEY = () => import.meta.env.VITE_APP_APPLICATION_SERVER_KEY || getValue(APP_SERVER_KEY);
 export const appTag = () => getValue(TAG_KEY);
