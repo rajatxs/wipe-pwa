9ecd9acf23c12295d2462f753b78362c1667c75d
Rajat Sharma <rxx175@gmail.com>
2023-02-28 21:52:30 +0530

    added PRESENCE_VIEW_MODE and appPresenceViewMode

--- src/config.js
+++ src/config.js
@@ -6,8 +6,10 @@ export const TAG_KEY = 'tag';
 export const AUTH_TOKEN_KEY = 'token';
 export const APP_SERVER_KEY = 'app-server-key';
 export const WA_SERVICE_RESTART_TS_KEY = 'wa-restart-ts';
+export const PRESENCE_VIEW_MODE = 'presence-view';
 
 export const APP_APPLICATION_SERVER_KEY = () => import.meta.env.VITE_APP_APPLICATION_SERVER_KEY || getValue(APP_SERVER_KEY);
 export const appTag = () => getValue(TAG_KEY);
 export const appAuthToken = () => getValue(AUTH_TOKEN_KEY);
 export const appRemoteURL = () => getValue(REMOTE_URL_KEY) || window.location.origin;
+export const appPresenceViewMode = () => getValue(PRESENCE_VIEW_MODE) || 'default';
