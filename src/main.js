4af844c2b7eeb9039e2f5b2e877703c7884a80aa
Rajat Sharma <chorizo.revamps2d@icloud.com>
2025-04-19 14:23:07 +0530

    Remove unused component registrations in main.js to streamline the application and eliminate dead code.

--- src/main.js
+++ src/main.js
@@ -1,21 +1,10 @@
-// @ts-nocheck
 import { createApp } from 'vue';
 import { VueQueryPlugin } from '@tanstack/vue-query';
 import App from './App.vue';
 import router from './router';
-import Button from './components/Button.vue';
-import IconButton from './components/IconButton.vue';
-import Switch from './components/Switch.vue';
-import Loader from './components/Loader.vue';
-
 import './app.css';
 
 const app = createApp(App);
-app.component('app-button', Button);
-app.component('app-icon-button', IconButton);
-app.component('app-switch', Switch);
-app.component('app-loader', Loader);
-
 app.use(router);
 app.use(VueQueryPlugin);
 app.mount('#app');
