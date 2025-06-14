5fe3e1a87e5aa53d1f7a7f62877a8540128c01d3
Rajat Sharma <rxx256@icloud.com>
2025-06-14 19:24:06 +0530

    Add database backup functionality: Implemented a button to download a database backup file, enhancing data management options in the settings view.

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,18 +1,47 @@
 <script setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import moment from 'moment';
 import appState from '../state';
 import store from '../utils/store';
+import { fetchDatabaseBackupFile } from '../api/common';
 import AboutDialog from '../components/AboutDialog.vue';
 import Switch from '../components/Switch.vue';
 
 const router = useRouter();
+const downloadingBackup = ref(false);
 const viewAboutDialog = ref(false);
 
 /** Sets view all subscriptions event */
 function setViewAllSubscriptions(value) {
     store.viewAllSubscriptions = value;
     appState.viewAllSubscriptions = value;
+}
+
+/** Downloads the database backup file */
+async function downloadBackup() {
+    downloadingBackup.value = true;
+
+    try {
+        const blob = await fetchDatabaseBackupFile();
+        const url = window.URL.createObjectURL(blob);
+
+        // Create a temporary <a> element to trigger the download
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = `Wipe Backup - ${moment().format('DD MMM YYYY')}.db`;
+        document.body.appendChild(link);
+        link.click();
+
+        // Clean up
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(url);
+    } catch (error) {
+        console.error(error);
+        alert('Unable to download backup');
+    }
+
+    downloadingBackup.value = false;
 }
 
 /** Clears saved data from the browser */
@@ -36,6 +65,7 @@ function logout() {
 
 <template>
     <div class="settings-view flex flex-col p-5 gap-3">
+        <!-- Show all subscriptions -->
         <div class="app-setting-item flex flex-row justify-between items-center" role="button" id="clear-data-setting">
             <div>
                 <h5 class="app-setting-item__title">Show all subscriptions</h5>
@@ -48,25 +78,44 @@ function logout() {
                 :value="appState.viewAllSubscriptions"
                 @value="setViewAllSubscriptions" />
         </div>
-        <div class="app-setting-item" role="button" id="clear-data-setting" @click="clearData">
+
+        <!-- Download backup -->
+        <button
+            class="app-setting-item"
+            id="download-backup-setting"
+            :disabled="downloadingBackup"
+            @click="downloadBackup">
+            <h5 class="app-setting-item__title">
+                {{ downloadingBackup ? 'Downloading backup...' : 'Download backup' }}
+            </h5>
+            <span class="app-setting-item__desc">Download a database backup of all application data</span>
+        </button>
+
+        <!-- Clear cache -->
+        <button class="app-setting-item" id="clear-data-setting" @click="clearData">
             <h5 class="app-setting-item__title">Clear cache</h5>
             <span class="app-setting-item__desc">Clear saved data from the browser</span>
-        </div>
-        <div class="app-setting-item" role="button" id="about-setting" @click="viewAboutDialog = true">
+        </button>
+
+        <!-- About -->
+        <button class="app-setting-item" id="about-setting" @click="viewAboutDialog = true">
             <h5 class="app-setting-item__title">About</h5>
             <span class="app-setting-item__desc">App version and other information</span>
-        </div>
-        <div class="app-setting-item" role="button" id="logout-setting" @click="logout">
+        </button>
+
+        <!-- Logout -->
+        <button class="app-setting-item" role="button" id="logout-setting" @click="logout">
             <h5 class="app-setting-item__title">Logout</h5>
             <span class="app-setting-item__desc">Logout from the current session</span>
-        </div>
+        </button>
     </div>
+
     <AboutDialog v-if="viewAboutDialog" @close="viewAboutDialog = false" />
 </template>
 
 <style>
 .app-setting-item {
-    @apply flex flex-col justify-center p-3 cursor-pointer border rounded-lg border-neutral-200 active:bg-neutral-100 md:hover:bg-neutral-100 dark:border-neutral-700 dark:active:bg-neutral-800 md:dark:hover:bg-neutral-800;
+    @apply flex flex-col justify-center items-start p-3 cursor-pointer border rounded-lg border-neutral-200 disabled:pointer-events-none disabled:opacity-50 active:bg-neutral-100 md:hover:bg-neutral-100 dark:border-neutral-700 dark:active:bg-neutral-800 md:dark:hover:bg-neutral-800;
 }
 
 .app-setting-item__title {
